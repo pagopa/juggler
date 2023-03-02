85608772e1dd7c4a7b3aa950bbf59904e74f9488
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-03-02 11:24:34 +0100

    Improve documentation

--- src/adapters/openapi/getOpenApiSpec.ts
+++ src/adapters/openapi/getOpenApiSpec.ts
@@ -1,14 +1,20 @@
 import fs from 'fs';
 import * as TE from 'fp-ts/TaskEither';
+import * as T from 'fp-ts/Task';
 import { flow, pipe } from 'fp-ts/lib/function';
 import SwaggerParser from '@apidevtools/swagger-parser';
-import * as T from 'fp-ts/Task';
 
 type FetchedContent = {
   content: string;
   fromOrigin: boolean;
 };
 
+/**
+ * Try to bundle the OpenAPI specification from the specified URL.
+ *
+ * @param url The URL of the OpenAPI specification
+ * @returns The bundled OpenAPI specification (as string) if the URL is valid, or an {@link Error} otherwise.
+ */
 const tryToBundleOpenApi = (
   url: string
 ): TE.TaskEither<Error, FetchedContent> =>
@@ -23,12 +29,21 @@ const tryToBundleOpenApi = (
     )
   );
 
+/**
+ * Fetch the OpenAPI specification from the specified URL.
+ * If the URL is not valid, or doesn't point to a valid OpenAPI specification,
+ * then the bundled OpenAPI specification returned are the specifications that belong
+ * to the Juggler.
+ *
+ * @param url The URL of the OpenAPI specification
+ * @returns A {@link T.Task} containing the {@link FetchedContent}.
+ */
 export const fetchOpenApiContent = (url: string) =>
   pipe(
     tryToBundleOpenApi(url),
     TE.getOrElse(() =>
       T.of({
-        content: fs.readFileSync('docs/openapi/juggler.yaml', 'utf8'),
+        content: fs.readFileSync('docs/openapi/juggler.yaml', 'utf8'), // TODO: Refactor using IO
         fromOrigin: false,
       })
     )
