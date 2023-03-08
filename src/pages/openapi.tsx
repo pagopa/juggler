453601246fb08254453f9a507baa4bfc64b1ba96
AF <alessandro.ferlin@pagopa.it>
2023-03-08 18:43:08 +0100

    Simplify openapi page

--- src/pages/openapi.tsx
+++ src/pages/openapi.tsx
@@ -1,17 +1,6 @@
-import * as T from 'fp-ts/Task';
-import { GetServerSideProps } from 'next';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
-type ApiViewerProps = {
-  url?: string;
-};
-
-export const getServerSideProps: GetServerSideProps<ApiViewerProps> = () =>
-  T.of({
-    props: { url: '/api/openapi' },
-  })();
-
-const ApiViewer = (props: ApiViewerProps) => <SwaggerUI url={props.url} />;
+const ApiViewer = () => <SwaggerUI url="/api/openapi" />;
 
 export default ApiViewer;
