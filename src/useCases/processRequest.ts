142922265721335deb1ad5ad2ff61caf3628422c
AF <alessandro.ferlin@pagopa.it>
2023-06-22 17:52:51 +0200

    Improve comment following code-review suggestion

--- src/useCases/processRequest.ts
+++ src/useCases/processRequest.ts
@@ -10,7 +10,8 @@ import { Capabilities } from '../domain/Capabilities';
 import { HttpRequest } from '../domain/RequestResponse';
 
 /**
- * Process the given request
+ * This function is the entry point for processing an HTTP request. Its goal is
+ * to deliver a dummy answer to the specified HTTP request.
  */
 export const processRequest = (request: HttpRequest) =>
   pipe(
