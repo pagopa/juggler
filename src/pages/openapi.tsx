b6a3b7e7e4fb064f2d9a27a998f376f64c4bd456
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-23 17:58:18 +0200

    Add basePath

--- src/pages/openapi.tsx
+++ src/pages/openapi.tsx
@@ -4,6 +4,7 @@ import '@stoplight/elements/styles.min.css';
 const ApiViewer = () => (
   <API
     apiDescriptionUrl="/api/openapi"
+    basePath={'/ui/openapi'}
     router={typeof window === 'undefined' ? 'memory' : 'history'}
     hideInternal={true}
     hideExport={true}
