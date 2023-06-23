42f332c69ace0e77a8867c9c215a36d41d8d935d
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-23 17:56:37 +0200

    Call useCase within the adapter

--- src/adapters/express/getOpenApiRouter.ts
+++ src/adapters/express/getOpenApiRouter.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import { pipe } from 'fp-ts/lib/function';
-import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import SwaggerParser from '@apidevtools/swagger-parser';
+import { getOpenApiSpec } from '../../useCases/getOpenApiSpec';
 import { AppEnv } from './AppEnv';
 import { problemDetail500 } from './errors';
 
@@ -12,7 +11,7 @@ export const makeGetOpenApiRouter = (env: AppEnv): express.Router => {
   // In the future this feature can be mapped into the domain
   router.get('/api/openapi', (_req, res) =>
     pipe(
-      TE.tryCatch(() => SwaggerParser.validate(env.openapi.URL), E.toError),
+      getOpenApiSpec(env.server)(env.openapi.URL),
       TE.bimap(
         () => res.status(500).json(problemDetail500),
         (openapi) => res.status(200).json(openapi)
